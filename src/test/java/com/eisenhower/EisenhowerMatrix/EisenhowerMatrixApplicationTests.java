7139a1c9ce3a7cdb6e370af9e19719d6fd08071f
lniemczy <lniemczy@wszib.edu.pl>
2020-04-17 19:55:34 +0200

    Initial commit

---
+++ src/test/java/com/eisenhower/EisenhowerMatrix/EisenhowerMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eisenhower.EisenhowerMatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EisenhowerMatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
